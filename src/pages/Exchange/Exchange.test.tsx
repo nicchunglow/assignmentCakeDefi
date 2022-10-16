fbbe54d773ab7e86aa42ba9e0e1ff77cf0896e83
nicchunglow <iamnicholaschung@gmail.com>
2022-10-17 00:50:54 +0800

    Update test description

--- src/pages/Exchange/Exchange.test.tsx
+++ src/pages/Exchange/Exchange.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Exchange from 'pages/Exchange';
 
-describe('Home', () => {
-  describe('Workshop', () => {
-    test('should render workshop', () => {
+describe('Exchange', () => {
+  describe('Header', () => {
+    test('should render', () => {
       render(<Exchange />);
       const ExchangeHeaderText = screen.getByLabelText('exchange-header-text');
       expect(ExchangeHeaderText).toBeInTheDocument();
